51451f037bd155b5d04d3f9e87d39bc47a67a2f6
Arjun31415 <arjunp0710@gmail.com>
2022-11-08 21:55:08 +0530

    somewhat working lsp

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,18 @@ use lapce_plugin::{
     register_plugin, Http, LapcePlugin, VoltEnvironment, PLUGIN_RPC,
 };
 use serde_json::Value;
+use std::fs::File;
+use zip::ZipArchive;
 
 #[derive(Default)]
 struct State {}
 
 register_plugin!(State);
-
+macro_rules! string {
+    ( $x:expr ) => {
+        String::from($x)
+    };
+}
 fn initialize(params: InitializeParams) -> Result<()> {
     let document_selector: DocumentSelector = vec![DocumentFilter {
         // lsp language id
@@ -33,7 +39,6 @@ fn initialize(params: InitializeParams) -> Result<()> {
     // serverPath = "[path or filename]"
     // serverArgs = ["--arg1", "--arg2"]
     // ```
-    // get latest version of tabnine
     if let Some(options) = params.initialization_options.as_ref() {
         if let Some(lsp) = options.get("lsp") {
             if let Some(args) = lsp.get("serverArgs") {
@@ -66,13 +71,16 @@ fn initialize(params: InitializeParams) -> Result<()> {
         }
     }
     let url = "https://update.tabnine.com/bundles/version";
-    PLUGIN_RPC.stderr("Helo");
+    PLUGIN_RPC.stderr("Starting tabnine version check");
     let mut resp = Http::get(url)?;
-    if resp.status_code.is_success() {
+    let version = if resp.status_code.is_success() {
         let body = resp.body_read_all()?;
-        let converted_body = std::str::from_utf8(&body).unwrap();
-        PLUGIN_RPC.stderr(&format!("Tabine Version: {}", converted_body));
-    }
+        String::from_utf8(body).unwrap()
+    } else {
+        panic!("Response error: {}", resp.status_code);
+    };
+    PLUGIN_RPC.stderr(&format!("Tabine Latest Version: {}", version));
+
     // Architecture check
     let platform = match VoltEnvironment::operating_system().as_deref() {
         Ok("macos") => "apple-darwin",
@@ -81,35 +89,64 @@ fn initialize(params: InitializeParams) -> Result<()> {
         p => panic!("unsupported platform {:#?}", p),
     };
 
-    let _ = match VoltEnvironment::architecture().as_deref() {
+    let arch = match VoltEnvironment::architecture().as_deref() {
         Ok("x86_64") => "x86_64",
         Ok("aarch64") => "aarch64",
-        _ => return Ok(()),
+        a => panic!("unsupported architecture {:#?}", a),
     };
-
-    // Download URL
-    // let _ = format!("https://github.com/<name>/<project>/releases/download/<version>/{filename}");
-
-    // see lapce_plugin::Http for available API to download files
-
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("windows") => {
-            format!("{}.exe", "[filename]")
+    let download_url = format!(
+        "https://update.tabnine.com/bundles/{}/{}-{}/TabNine.zip",
+        version, arch, platform
+    );
+    PLUGIN_RPC.stderr(&format!("Downloading Tabnine from {}", download_url));
+    let mut resp = Http::get(&download_url)?;
+    let zip_file = format!("TabNine-{}-{}-{}.zip", version, arch, platform);
+    if resp.status_code.is_success() {
+        let body = resp.body_read_all()?;
+        std::fs::write(&zip_file, body).unwrap();
+        let mut zip = ZipArchive::new(File::open(&zip_file).unwrap()).unwrap();
+        // for every zip file
+        for i in 0..zip.len() {
+            let mut file = zip.by_index(i).unwrap();
+            let outpath = match file.enclosed_name() {
+                Some(path) => path.to_owned(),
+                None => continue,
+            };
+            PLUGIN_RPC.stderr(&format!("Extracting {}", outpath.display()));
+            if (*file.name()).ends_with('/') {
+                std::fs::create_dir_all(&outpath).unwrap();
+            } else {
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        std::fs::create_dir_all(p).unwrap();
+                    }
+                }
+                let mut outfile = std::fs::File::create(&outpath).unwrap();
+                std::io::copy(&mut file, &mut outfile).unwrap();
+            }
         }
-        _ => "[filename]".to_string(),
+        std::fs::remove_file(&zip_file).unwrap();
+    } else {
+        panic!("Response error: {}", resp.status_code);
+    }
+    PLUGIN_RPC.stderr("Finished extraction");
+    let filename = match VoltEnvironment::operating_system().as_deref() {
+        Ok("windows") => {
+            string!("TabNine.exe")
+        }
+        _ => string!("TabNine"),
     };
+    // see lapce_plugin::Http for available API to download files
 
     // Plugin working directory
     let volt_uri = VoltEnvironment::uri()?;
-    let server_uri = Url::parse(&volt_uri)?.join("[filename]")?;
-
-    // if you want to use server from PATH
-    // let server_uri = Url::parse(&format!("urn:{filename}"))?;
+    let server_path = Url::parse(&volt_uri)?.join(&filename)?;
 
     // Available language IDs
     // https://github.com/lapce/lapce/blob/HEAD/lapce-proxy/src/buffer.rs#L173
+    PLUGIN_RPC.stderr("Starting Tabnine LSP");
     PLUGIN_RPC.start_lsp(
-        server_uri,
+        server_path,
         server_args,
         document_selector,
         params.initialization_options,
